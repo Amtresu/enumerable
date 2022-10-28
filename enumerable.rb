8e672de3b45040a0f82ac0440f34b9dee9f47abf
Amtresu <conor.follette@gmail.com>
2022-10-28 20:19:46 +0900

    fixed linter errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,18 +1,17 @@
 module MyEnumerable
-    def all?
-      each { |item| return false unless yield item }
-      true
-    end
-  
-    def any?
-      each { |item| return true if yield item }
-      false
-    end
-  
-    def filter?
-      val = []
-      each { |item| val.push(item) if yield item }
-      val
-    end
+  def all?
+    each { |item| return false unless yield item }
+    true
   end
-  
+
+  def any?
+    each { |item| return true if yield item }
+    false
+  end
+
+  def filter?
+    val = []
+    each { |item| val.push(item) if yield item }
+    val
+  end
+end
